0f123ed9980fe599db0973570a6f6173f59a9c4a
tedborges <tedborges.atendimento@gmail.com>
2025-03-24 00:05:44 -0300

    menu mobile refatorado e erro do animascroll func

--- js/modules/scroll-anima.js
+++ js/modules/scroll-anima.js
@@ -5,7 +5,7 @@ export default class ScrollAnima {
     this.sections = document.querySelectorAll(sections);
     this.windowMetade = window.innerHeight * 0.6;
 
-    this.animaScroll = debounce(this.animaScroll.bind(this), 50);
+    this.checkDistance = debounce(this.checkDistance.bind(this), 50);
   }
 
   /**
@@ -27,9 +27,9 @@ export default class ScrollAnima {
   checkDistance() {
     this.distance.forEach((item) => {
       if (window.pageYOffset > item.offset) {
-        section.classList.add('ativo');
-      } else if (item.element.section.classList.contains('ativo')) {
-        item.element.section.classList.remove('ativo');
+        item.element.classList.add('ativo');
+      } else if (item.element.classList.contains('ativo')) {
+        item.element.classList.remove('ativo');
       }
     })
   }
@@ -38,13 +38,13 @@ export default class ScrollAnima {
     if (this.sections.length) {
       this.getDistance();
       this.checkDistance();
-      window.addEventListener('scroll', this.animaScroll);
+      window.addEventListener('scroll', this.checkDistance);
     }
     return this;
   }
 
   // remove o event de scroll
   stop() {
-    window.removeEventListener('scroll', this.animaScroll);
+    window.removeEventListener('scroll', this.checkDistance);
   }
 }
--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ import Tooltip from './modules/tooltip.js';
 import DropdownMenu from './modules/dropdown-menu.js';
 import fetchBitcoin from './modules/fetch-bitcoin.js';
 import fetchAnimais from './modules/fetch-animais.js';
-import initMenuMobile from './modules/menu-mobile.js';
+import MenuMobile from './modules/menu-mobile.js';
 import initFuncionamento from './modules/funcionamento.js';
 import ScrollAnima from './modules/scroll-anima.js';
 
@@ -31,10 +31,12 @@ scrollAnima.init();
 const dropdownMenu = new DropdownMenu('[data-dropdown]');
 dropdownMenu.init();
 
+const menuMobile = new MenuMobile('[data-menu="button"]', '[data-menu="list"]');
+menuMobile.init();
+
+initFuncionamento();
+
 fetchAnimais('../animaisapi.json', '.numeros-grid');
 
 fetchBitcoin('https://blockchain.info/ticker', '.btc-preco');
 
-
-initMenuMobile();
-initFuncionamento();
