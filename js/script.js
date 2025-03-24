3057e998d3ab7e87717fb940f81143ffba05ca07
tedborges <tedborges.atendimento@gmail.com>
2025-03-23 23:32:14 -0300

    dropdown menu refatorado

--- js/script.js
+++ js/script.js
@@ -3,9 +3,9 @@ import Accordion from './modules/accordion.js';
 import TabNav from './modules/tabnav.js';
 import Modal from './modules/modal.js';
 import Tooltip from './modules/tooltip.js';
-import fetchAnimais from './modules/fetch-animais.js';
+import DropdownMenu from './modules/dropdown-menu.js';
 import fetchBitcoin from './modules/fetch-bitcoin.js';
-import initDropdownMenu from './modules/dropdown-menu.js';
+import fetchAnimais from './modules/fetch-animais.js';
 import initMenuMobile from './modules/menu-mobile.js';
 import initFuncionamento from './modules/funcionamento.js';
 import ScrollAnima from './modules/scroll-anima.js';
@@ -28,10 +28,13 @@ tooltip.init();
 const scrollAnima = new ScrollAnima('[data-anime="scroll"]');
 scrollAnima.init();
 
+const dropdownMenu = new DropdownMenu('[data-dropdown]');
+dropdownMenu.init();
+
 fetchAnimais('../animaisapi.json', '.numeros-grid');
 
 fetchBitcoin('https://blockchain.info/ticker', '.btc-preco');
 
-initDropdownMenu();
+
 initMenuMobile();
 initFuncionamento();
